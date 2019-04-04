65fc9e2a363f8de6986485254cac7db2212581d5
Sergio Casero hernández <sergio.casero23@gmail.com>
2019-04-04 20:42:59 +0200

    Added Result structure

--- common/src/commonMain/kotlin/com/mpp/seed/common/Result.kt
+++ common/src/commonMain/kotlin/com/mpp/seed/common/Result.kt
@@ -1,11 +1,15 @@
 package com.mpp.seed.common
 
 sealed class Result {
-    data class List<T>(val data: kotlin.collections.List<T>) : Result()
-    data class Single<T>(val data: T) : Result()
-    data class Update<T>(val data: T) : Result()
-    data class Token<T>(val data: T, val token: String) : Result()
-    data class Delete<T>(val data: Int) : Result()
+    class Success<T> : Result()
+    sealed class Error : Result() {
+        sealed class Network : Error() {
+            object NoInternet : Network()
+            object ServerNotResponding : Network()
+        }
 
-    data class Error(val code: Int, val message: String) : Result()
+        sealed class Local : Error() {
+            object Write : Local()
+        }
+    }
 }
